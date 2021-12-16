907462a9cfed1b5906f026d6358f526374ac232a
markk39006 <markk39006@gmail.com>
2021-12-16 12:32:11 +0700

    com

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string skillname;
+            string check;
+            Console.WriteLine("input skill name");
+            skillname = Console.ReadLine();
+            Skill skillName1 = new Skill(skillname);
+            _91(skillname);
+            check = Console.ReadLine();
+            if(check == "Y")
+            {
+
+            }
+            else if(check == "N")
+            {
+                _91(skillname);
+            }
+        }
+
+        static void _91(string skillname)
+        {
+            if (skillname == "?")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Add dependency for {0}", skillname);
+            }
+        }
+
+        static void _93(string skillname)
+        {
+            Console.WriteLine("inpu skill name");
+        }
+    }
+
+    class Skill
+    {
+        public Skill left;
+        public Skill right;
+        public string SkillName;
+
+        public Skill(string skillnameValue)
+        {
+            SkillName = skillnameValue;
+        }
+    }
+}
